f79f8d381f10290d7f1aac48acfdcd1c3a392363
TareqMonwer <tareqmonwer137@gmail.com>
2019-12-20 04:49:28 +0600

    gunicorn setup

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'mysite.com',
+    '*'
 ]
 
 # Application definition
@@ -95,14 +96,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'sms-lio',
-    #     'USER': 'postgres',
-    #     'PASSWORD': '123456',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
