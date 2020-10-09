219b96a52de3f3a187d6865f5dc0f4b6dfca7538
TareqMonwer <tareqmonwer137@gmail.com>
2020-10-10 05:21:12 +0600

    online applicant list in student app dashboard

--- students/urls.py
+++ students/urls.py
@@ -4,9 +4,12 @@ from . import views
 app_name = 'students'
 
 urlpatterns = [
-     path('', views.students_dashboard_index, name='students_dashboard_index'),
+     path('', views.students_dashboard_index, 
+          name='students_dashboard_index'),
      path('all/', views.students_view, name='all_student'),
      path('add/', views.add_student_view, name='add_student'),
+     path('online-applicants/', views.online_applicants_list, 
+          name='online_applicants_list'),
      path('update/<int:pk>/', views.student_update_view.as_view(),
           name='update_student'),
      path('<int:pk>/detail/', views.student_detail_view.as_view(),
@@ -15,6 +18,6 @@ urlpatterns = [
      path('<int:pk>/students/', views.students_by_department_view,
           name='students_by_dept'),
      path('result/', views.student_result_view, name='result'),
-     path('add_result_from_student/<int:pk>/', views.add_result_from_student_detail_view,
-	name='add_result_in_details'),
+     path('add_result_from_student/<int:pk>/', views.add_result_from_student_detail_view, 
+          name='add_result_in_details'),
 ]
