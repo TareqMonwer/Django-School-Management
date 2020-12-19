eba2da629488f01c30e95ff9efbb5dcc3033429d
TareqMonwer <tareqmonwer137@gmail.com>
2020-12-19 19:27:15 +0600

    added result model and subjects fixtures

--- result/admin.py
+++ result/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+
+from .models import Result
+
+
+class ResutlAdmin(admin.ModelAdmin):
+    list_display = ('student', 'semester', 'subject', 'total_marks')
+
+
+admin.site.register(Result, ResutlAdmin)
--- result/models.py
+++ result/models.py
@@ -1,4 +1,59 @@
-from django.db import models
-from django.conf import settings
 from model_utils.models import TimeStampedModel
 
+from django.db import models
+from django.conf import settings
+
+from students.models import Student
+from academics.models import Subject, Semester
+
+
+class Exam(TimeStampedModel):
+    EXAM_CHOICES = (
+        ('Mid Term', 'm'),
+        ('Final', 'f')
+    )
+    exam_name = models.CharField(
+        max_length=1,
+        choices=EXAM_CHOICES
+    )
+    exam_date = models.DateTimeField()
+
+
+class Result(TimeStampedModel):
+    student = models.ForeignKey(
+        Student,
+        on_delete=models.CASCADE,
+        related_name='results'
+    )
+    semester = models.ForeignKey(
+        Semester,
+        on_delete=models.CASCADE
+    )
+    subject = models.ForeignKey(
+        Subject,
+        on_delete=models.CASCADE
+    )
+    practical_marks = models.SmallIntegerField(
+        blank=True,
+        null=True
+    )
+    theory_marks = models.SmallIntegerField(
+        blank=True,
+        null=True
+    )
+    total_marks = models.SmallIntegerField(
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return f'{self.student} | {self.subject} | {self.total_marks}'
+    
+    def save(self, *args, **kwargs):
+        if self.theory_marks and self.practical_marks:
+            self.total_marks = self.practical_marks + self.theory_marks
+        elif self.practical_marks and not self.theory_marks:
+            self.total_marks = self.practical_marks
+        else:
+            self.total_marks = self.theory_marks
+        super().save(*args, **kwargs)
