29bc4511517f46ae3c847dd69c45e79cc924188d
tareqmonwer <tareqmonwer137@gmail.com>
2020-12-20 02:43:57 +0600

    resutl detail template, route and view init

--- result/views.py
+++ result/views.py
@@ -8,3 +8,7 @@ def result_view(request):
     f = ResultFilter(request.GET, queryset=Result.objects.all())
     ctx = {'filter': f,}
     return render(request, 'result/result_filter.html', ctx)
+
+
+def result_detail_view(request, student_pk):
+    return render(request, 'result/result_detail.html')
