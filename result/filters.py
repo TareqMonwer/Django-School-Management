b8aa80559e3ecac0a6bead61efde224ce3ea4a7b
tareqmonwer <tareqmonwer137@gmail.com>
2020-12-20 01:03:06 +0600

    result filter by student temporary id

--- result/filters.py
+++ result/filters.py
@@ -3,6 +3,14 @@ import django_filters
 from .models import Result
 
 class ResultFilter(django_filters.FilterSet):
+    student__temporary_id = django_filters.CharFilter(
+        lookup_expr='icontains',
+        label='Temporary ID'
+    )
     class Meta:
         model = Result
-        fields = ['semester', 'subject',]
+        fields = [
+            'semester',
+            'subject',
+            'student__temporary_id'
+        ]
