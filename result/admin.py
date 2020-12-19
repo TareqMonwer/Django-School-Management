16ee8c19eea11f05026ccf0002282e2140638ea4
tareqmonwer <tareqmonwer137@gmail.com>
2020-12-20 01:57:37 +0600

    filter resutls by exam

--- result/admin.py
+++ result/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
 
-from .models import Result
+from .models import Result, Exam
 
 
 class ResutlAdmin(admin.ModelAdmin):
     list_display = (
-        'student', 'semester',
+        'student', 'semester', 'exam',
         'subject', 'theory_marks',
         'practical_marks', 'total_marks'
     )
-    list_editable = ('total_marks',)
+    list_editable = ('total_marks', 'exam')
+
+
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('exam_name', 'exam_date')
 
 
 admin.site.register(Result, ResutlAdmin)
+admin.site.register(Exam, ExamAdmin)
