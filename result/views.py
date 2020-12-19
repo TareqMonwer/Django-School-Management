72332bdf5bd2918f5886789d2b7f51d8374cfc55
TareqMonwer <tareqmonwer137@gmail.com>
2020-12-19 20:13:39 +0600

    django-filter init

--- result/views.py
+++ result/views.py
@@ -1,4 +1,10 @@
+from django.shortcuts import render
+
+from .models import Result
+from .filters import ResultFilter
 
 
-
-
+def result_view(request):
+    f = ResultFilter(request.GET, queryset=Result.objects.all())
+    ctx = {'form': f, }
+    return render(request, 'result/result_filter.html', ctx)
