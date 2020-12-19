1548cbe750113a107af3bcffafed14e2ae83516e
tareqmonwer <tareqmonwer137@gmail.com>
2020-12-20 02:43:57 +0600

    resutl detail template, route and view init

--- result/urls.py
+++ result/urls.py
@@ -6,4 +6,7 @@ app_name = 'result'
 
 urlpatterns = [
     path('', views.result_view, name='result_home'),
+    path('student/<int:student_pk>/', views.result_detail_view,
+        name='result_detail_view'
+    ),
 ]
