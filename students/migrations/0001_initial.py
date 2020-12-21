e77d2c6d78757522c061b60e7bc58530e348c642
tareqmonwer <tareqmonwer137@gmail.com>
2020-12-21 12:47:31 +0600

    add SubjectGroup models to result app

--- students/migrations/0001_initial.py
+++ students/migrations/0001_initial.py
@@ -13,7 +13,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('admin_tools', '0001_initial'),
     ]
 
     operations = [
