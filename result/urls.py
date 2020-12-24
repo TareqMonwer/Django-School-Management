946d602369f1fd425ae9ce257e77828e4a2b155b
tareqmonwer <tareqmonwer137@gmail.com>
2020-12-25 01:27:18 +0600

    Create Multiple Selectable Items with JS

--- result/urls.py
+++ result/urls.py
@@ -6,6 +6,9 @@ app_name = 'result'
 
 urlpatterns = [
     path('', views.result_view, name='result_home'),
+    path('subject-groups/', views.create_subject_group,
+        name='subject_groups'
+    ),
     path('student/<int:student_pk>/', views.result_detail_view,
         name='result_detail_view'
     ),
