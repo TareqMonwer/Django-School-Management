d2958a4f4aef339def21e674c55928b35d89908d
tareqmonwer <tareqmonwer137@gmail.com>
2021-01-26 05:03:01 +0600

    adding institute widgets

--- institute/admin.py
+++ institute/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import InstituteProfile
+from .models import (
+    InstituteProfile, TextWidget,
+    ListWidget, WidgetListItem
+)
 
 
 admin.site.register(InstituteProfile)
+admin.site.register(TextWidget)
+admin.site.register(ListWidget)
+admin.site.register(WidgetListItem)
