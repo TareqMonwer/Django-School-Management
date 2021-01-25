20fe878be1d39f184cdc64b5dd67a38a98001f48
tareqmonwer <tareqmonwer137@gmail.com>
2021-01-25 20:06:17 +0600

    blog dynamic navbar init

--- context_processors/dj_sms_context_processor.py
+++ context_processors/dj_sms_context_processor.py
@@ -1,5 +1,12 @@
 from institute.models import InstituteProfile
+from articles.models import Category
 
 def attach_institute_data_ctx_processor(request):
     institute = InstituteProfile.objects.get(active=True)
-    return {'request_institute': institute, }
+    registered_navlinks = Category.objects.filter(
+        display_on_menu=True,
+    ).order_by('-created')
+    return {
+        'request_institute': institute,
+        'registered_navlinks': registered_navlinks
+    }
