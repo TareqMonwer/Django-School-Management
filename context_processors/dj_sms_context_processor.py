d73de1a558089ddf9e7ed6ef16ac23a80e010a42
tareqmonwer <tareqmonwer137@gmail.com>
2021-01-26 00:28:12 +0000

    update template and context-processor for artilces footer widgets

--- context_processors/dj_sms_context_processor.py
+++ context_processors/dj_sms_context_processor.py
@@ -1,9 +1,11 @@
-from institute.models import InstituteProfile
+from institute.models import (InstituteProfile,
+        TextWidget, ListWidget
+)
 from articles.models import Category
 
 def attach_institute_data_ctx_processor(request):
     institute = InstituteProfile.objects.get(active=True)
-
+    
     ctx = {
         'request_institute': institute,
     }
@@ -13,5 +15,16 @@ def attach_institute_data_ctx_processor(request):
         registered_navlinks = Category.objects.filter(
             display_on_menu=True,
         ).order_by('-created')
+        try:
+            # first text-widget for the footer (footer-col-number-1)
+            first_widget =  TextWidget.objects.get(widget_number=0)
+            # get rest three widgets
+            list_widgets = ListWidget.objects.filter(widget_number__lte=4)
+        except (TextWidget.DoesNotExist, Exception):
+            first_widget = None
+            list_widgets = None
+
         ctx['registered_navlinks'] = registered_navlinks
+        ctx['first_widget'] = first_widget
+        ctx['list_widgets'] = list_widgets
     return ctx
