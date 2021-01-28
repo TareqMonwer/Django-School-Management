ae59ea627570aa67504e01f0a7ed45c66c15cdcf
tareqmonwer <tareqmonwer137@gmail.com>
2021-01-29 04:13:48 +0600

    :construction: user profile integration and edit profile WIP

--- articles/views.py
+++ articles/views.py
@@ -2,7 +2,7 @@ from braces.views import LoginRequiredMixin
 
 from itertools import chain
 
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.shortcuts import redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views import View
@@ -13,6 +13,9 @@ from django.views.generic import (
 )
 
 from accounts.models import User
+from accounts.forms import (
+    CommonUserProfileForm, UserProfileSocialLinksFormSet
+)
 from .models import Article, Like, Category
 from .mixins import AuthorArticleEditMixin
 from .forms import ArticleForm
@@ -143,3 +146,32 @@ class AuthorProfile(DetailView):
     
     def get_slug_field(self):
         return 'username'
+    
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        profile_edit_form = CommonUserProfileForm()
+        ctx['profile_edit_form'] = profile_edit_form
+        formset = UserProfileSocialLinksFormSet(instance=self.request.user.profile)
+        ctx['social_links_form'] = formset
+        return ctx
+    
+    def post(self, request, *args, **kwargs):
+        profile_form = CommonUserProfileForm(
+            request.POST,
+            request.FILES,
+            instance=self.request.user.profile
+        )
+        social_formset = UserProfileSocialLinksFormSet(
+            request.POST, instance=self.request.user.profile
+        )
+        if profile_form.is_valid():
+            profile_form.save()
+
+            if social_formset.is_valid():
+                social_formset.save()
+                return HttpResponse('social and profile updated')
+            else:
+                return HttpResponse('only profile updated')
+        else:
+            context = super().get_context_data(**kwargs)
+            return self.render_to_response(context=context)
