9b74cdf97e5141a67d415c7d56d53e55ffea9098
tareqmonwer <tareqmonwer137@gmail.com>
2021-01-29 18:58:39 +0600

    :construction: making set-up process error-free [WIP]

--- institute/middleware.py
+++ institute/middleware.py
@@ -5,13 +5,20 @@ class AttachInstituteDataMiddleware:
 
     def __init__(self, get_response):
         self.get_response = get_response
-        institute = InstituteProfile.objects.get(active=True)
-        self.institute = institute
+        try:
+            institute = InstituteProfile.objects.get(active=True)
+            self.institute = institute
+        except:
+            pass
 
     def __call__(self, request):
         response = self.get_response(request)
         return response
     
     def process_template_response(self, request, response):
-        response.context_data["request_institute"] = self.institute
-        return response
+        try:
+            response.context_data["request_institute"] = self.institute
+            return response
+        except django.db.utils.OperationalError:
+            return response
+        
