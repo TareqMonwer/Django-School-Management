9ee6241d391bef4e1912c2db390e2aa38ad39384
tareqmonwer <tareqmonwer137@gmail.com>
2021-02-03 17:39:36 +0600

    Feat: Implement articles, instititute and accounts import-export in django-admind.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from rolepermissions.roles import assign_role
 from rolepermissions.admin import RolePermissionsUserAdminMixin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
@@ -11,8 +13,12 @@ from .forms import UserRegistrationForm, UserChangeForm
 
 User = get_user_model()
 
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
 
-class UserAdmin(RolePermissionsUserAdminMixin, auth_admin.UserAdmin):
+
+class UserAdmin(RolePermissionsUserAdminMixin, ImportExportModelAdmin, auth_admin.UserAdmin):
 
     form = UserChangeForm
     add_form = UserRegistrationForm
@@ -22,6 +28,7 @@ class UserAdmin(RolePermissionsUserAdminMixin, auth_admin.UserAdmin):
     list_display = ["username", "is_superuser", "approval_status", "requested_role"]
     list_editable = ["approval_status",]
     search_fields = ["approval_status", "requested_role"]
+    resource_class = UserResource
 
     # TODO: Assign users to requested group
     # def save_model(self, request, obj, form, change): 
--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,5 @@
 from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from mptt.admin import MPTTModelAdmin
 from mptt.models import TreeManyToManyField
 from django.forms import CheckboxSelectMultiple
@@ -6,8 +7,12 @@ from django.contrib import admin
 
 from .models import Article, Like, Category, Newsletter
 
+class ArticleResource(resources.ModelResource):
+    class Meta:
+        model = Article
 
-class ArticleAdmin(admin.ModelAdmin):
+
+class ArticleAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ['title', 'author', 'created',
         'force_highlighted', 'status'
     ]
@@ -20,18 +25,43 @@ class ArticleAdmin(admin.ModelAdmin):
     formfield_overrides = {
         TreeManyToManyField: {'widget': CheckboxSelectMultiple},
     }
+    resource_class = ArticleResource
 
 
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryResource(resources.ModelResource):
+    class Meta:
+        model = Category
+
+
+class CategoryAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ['name',]
+    resource_class = CategoryResource
+
+
+class NewsletterResource(resources.ModelResource):
+    class Meta:
+        model = Newsletter
+
+
+class NewsletterAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ['email', 'created']
+    resource_class = NewsletterResource
+
+
+class LikeResource(resources.ModelResource):
+    class Meta:
+        model = Like
+
+
+@admin.register(Like)
+class LikeAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    # TODO: Display obj __str__ representation in list display.
+    list_display = ['__str__', 'created']
+    resource_class = LikeResource
 
 
 # Registers
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Newsletter)
+admin.site.register(Newsletter, NewsletterAdmin)
 
-@admin.register(Like)
-class LikeAdmin(admin.ModelAdmin):
-    # TODO: Display obj __str__ representation in list display.
-    list_display = ['__str__', 'created']
