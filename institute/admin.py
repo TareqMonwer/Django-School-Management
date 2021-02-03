20e2dfd470fdc441f0f9e2c889c6ac52d61cb31c
tareqmonwer <tareqmonwer137@gmail.com>
2021-02-03 17:39:36 +0600

    Feat: Implement articles, instititute and accounts import-export in django-admind.

--- institute/admin.py
+++ institute/admin.py
@@ -1,3 +1,6 @@
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from django.contrib import admin
 
 from .models import (
@@ -6,17 +9,46 @@ from .models import (
 )
 
 
+class InstituteProfileResource(resources.ModelResource):
+    class Meta:
+        model = InstituteProfile
+
+
+class InstituteProfileAdmin(ImportExportModelAdmin):
+    resource_class = InstituteProfileResource
+
+
+class WidgetListItemResource(resources.ModelResource):
+    class Meta:
+        model = WidgetListItem
+
+
 class WidgetListItemInline(admin.TabularInline):
     model = WidgetListItem
 
 
-class ListWidgetAdmin(admin.ModelAdmin):
+class ListWidgetResource(resources.ModelResource):
+    class Meta:
+        model = ListWidget
+
+
+class ListWidgetAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     inlines = [
             WidgetListItemInline,
     ]
+    resource_class = ListWidgetResource
 
 
-admin.site.register(InstituteProfile)
-admin.site.register(TextWidget)
+class TextWidgetResource(resources.ModelResource):
+    class Meta:
+        model = TextWidget
+
+
+class TextWidgetAdmin(ImportExportModelAdmin):
+    resource_class = TextWidgetResource
+
+
+admin.site.register(InstituteProfile, InstituteProfileAdmin)
+admin.site.register(TextWidget, TextWidgetAdmin)
 admin.site.register(ListWidget, ListWidgetAdmin)
 admin.site.register(WidgetListItem)
