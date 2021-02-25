54ff7b9332e489d8b86789555f8706fb787639cb
tareqmonwer <tareqmonwer137@gmail.com>
2021-02-25 14:19:50 +0600

    storing payment details in database.

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import SSLPayment, SSLAdmissionPaymentVerfication
 
-# Register your models here.
+
+@admin.register(SSLPayment)
+class SSLPaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'transaction_id',
+        'payer',
+        'received_amount',
+        'pay_reason',
+        'payer_mobile',
+        'payer_email'
+    )
+
+
+@admin.register(SSLAdmissionPaymentVerfication)
+class SSLAdmissionPaymentVerficationAdmin(admin.ModelAdmin):
+    list_display = (
+        'payment',
+        'status',
+        'verified_by',
+    )
