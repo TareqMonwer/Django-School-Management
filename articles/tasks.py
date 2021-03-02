a5f31ad779fc1861d606801d0891e3674d525068
tareqmonwer <tareqmonwer137@gmail.com>
2021-03-02 17:32:18 +0600

    mail template created and seinding emails on publishing articles from dashboard.

--- articles/tasks.py
+++ articles/tasks.py
@@ -1,7 +1,10 @@
+from pathlib import Path
 from celery.decorators import task
 from celery.utils.log import get_logger
 
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
 from django.core import serializers
 from config.settings import EMAIL_HOST_USER
 from .models import Article
@@ -14,10 +17,15 @@ logger = get_logger(__name__)
 def send_latest_article(mail_list, article_id):
     article = Article.objects.get(id=article_id)
     deserialized_mail_list = serializers.deserialize("json", mail_list, ignorenonexistent=True)
-    send_mail(
-        f'Read Latest Article: {article.title}',
-        f'{article.title}',
-        EMAIL_HOST_USER,
-        [subscriber.object.email for subscriber in deserialized_mail_list], 
-        fail_silently=False
+
+    html_template = Path('articles/newsletter-template.html')
+    html_message = render_to_string(html_template, { 'article': article, })
+    
+    message = EmailMessage(
+        'DPI JUST PUBLISHED NEW ARTICLE! READ IT', 
+        html_message, 
+        EMAIL_HOST_USER, 
+        [subscriber.object.email for subscriber in deserialized_mail_list]
     )
+    message.content_subtype = 'html'
+    message.send()
