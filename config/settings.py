c53a4d6e7c73480b18d58ad106af812ed1c08ea2
tareqmonwer <tareqmonwer137@gmail.com>
2021-03-08 17:51:47 +0600

    remove password validators for the sake of lazy users.

--- config/settings.py
+++ config/settings.py
@@ -198,15 +198,6 @@ if 'mysql' in DATABASES['default']['ENGINE']:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
