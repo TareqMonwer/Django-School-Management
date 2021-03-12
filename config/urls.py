df541fd1f0b388ac3ef80dc31157875baeab23e0
tareqmonwer <tareqmonwer137@gmail.com>
2021-03-12 16:39:56 +0600

    serve staticfiles from same server.

--- config/urls.py
+++ config/urls.py
@@ -63,11 +63,10 @@ urlpatterns = [
     path('api/', include('articles.api.routes')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                            document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL,
-                            document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                        document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,
+                        document_root=settings.STATIC_ROOT)
 
 if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
