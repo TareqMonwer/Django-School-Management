f28dd3c58ce88a84ea7ba81f9625fb4ecf0ce5b3
tareqmonwer <tareqmonwer137@gmail.com>
2021-03-25 00:28:25 +0600

    create notices models and register them in django-admin.

--- notices/admin.py
+++ notices/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Notice, NotifyGroup, NoticeResponse
 
-# Register your models here.
+
+class NoticeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'uploaded_by')
+
+
+class NotifyGroupAdmin(admin.ModelAdmin):
+    list_display = ('group_name',)
+
+
+class NoticeResponseAdmin(admin.ModelAdmin):
+    list_display = ('notice', 'responder', 'created')
+
+
+admin.site.register(Notice, NoticeAdmin)
+admin.site.register(NotifyGroup, NotifyGroupAdmin)
+admin.site.register(NoticeResponse, NoticeResponseAdmin)
--- notices/models.py
+++ notices/models.py
@@ -1,3 +1,76 @@
+from model_utils.models import TimeStampedModel
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Notice(TimeStampedModel):
+    NOTICE_TYPES = (
+        ('h', 'Holiday'),
+        ('n', 'None')
+    )
+    title = models.CharField(max_length=255)
+    notice_type = models.CharField(
+        max_length=3,
+        choices=NOTICE_TYPES,
+        default='n'
+    )
+    file = models.FileField(
+        upload_to='files/notices/',
+        blank=True,
+        null=True
+    )
+    content = RichTextUploadingField(config_name='default')
+    uploaded_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='published_notices',
+        blank=True,
+        null=True
+    )
+    notfiy_groups = models.ManyToManyField(
+        'NotifyGroup',
+        related_name='notfied_notices'
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created', ]
+
+
+class NotifyGroup(TimeStampedModel):
+    group_name = models.CharField(max_length=55)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+    users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='notify_groups'
+    )
+
+    def __str__(self):
+        return self.group_name
+
+    class Meta:
+        ordering = ['group_name', '-created']
+
+
+class NoticeResponse(TimeStampedModel):
+    notice = models.ForeignKey(
+        Notice,
+        on_delete=models.CASCADE,
+        related_name='responses'
+    )
+    responder = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='notice_responses'
+    )
+
+    def __str__(self):
+        return f'{self.notice} - {self.responder}'
