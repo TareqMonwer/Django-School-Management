939d49de7bdcf8e6fe49bbac58f77bb8696d6c3e
tareqmonwer <tareqmonwer137@gmail.com>
2021-03-26 12:47:41 +0600

    record and display responses for notices.

--- notices/views/site_views.py
+++ notices/views/site_views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView, DetailView
-from notices.models import Notice
+from django.views.generic import FormView, ListView, DetailView
+from notices.models import Notice, NoticeResponse
 
 
 class NoticesPageView(ListView):
@@ -12,3 +12,16 @@ class NoticeDetailView(DetailView):
     model = Notice
     template_name = 'notices/site/detail.html'
     context_object_name = 'notice'
+
+    def get_context_data(self, *args, **kwargs):
+        user = self.request.user
+        context = super().get_context_data(*args, **kwargs)
+        if user.is_authenticated:
+            NoticeResponse.objects.get_or_create(
+                notice=context.get('object'),
+                responder=user
+            )
+            context['user_response_status'] = 'r'  # recorded
+        else:
+            context['user_response_status'] = 'nr'  # not recorded
+        return context
