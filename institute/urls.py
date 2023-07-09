52aac52f4372ee5a5f1b3b24e2e4d4e528017463
Tarek Monwer <tarek.monwer@iqvia.com>
2023-07-10 01:51:59 +0600

    add institute setup feature

--- institute/urls.py
+++ institute/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 app_name = 'institute'
 urlpatterns = [
+    path('dashboard/settings/setup-school/',
+        views.InstituteProfileSetupDashboard.as_view(),
+        name='setup_school'
+    ),
     path('dashboard/settings/<int:institute_pk>/', 
         views.InstituteProfileConfigDashboard.as_view(), 
         name='institute_config'
