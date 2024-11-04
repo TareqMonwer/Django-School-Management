9fc042608b3990ec6849341627b4150ee89b7f2d
TareqMonwer <tareqmonwer137@gmail.com>
2024-11-04 21:08:23 +0600

    update account creation process

---
+++ django_school_management/accounts/services/auth.py
@@ -0,0 +1,25 @@
+from django_school_management.accounts.constants import ProfileApprovalStatusEnum, AccountTypesEnum
+from django_school_management.accounts.models import CommonUserProfile, CustomGroup
+from django_school_management.accounts.request_context import RequestUserContext
+
+
+def create_profile_for_approved_account(user):
+    if user.approval_status == ProfileApprovalStatusEnum.approved.value:
+        profile, created = CommonUserProfile.objects.get_or_create(user=user)
+        return profile, created
+    return None, None
+
+
+def handle_superuser_creation(user):
+    create_profile_for_approved_account(user)
+    user.approval_status = ProfileApprovalStatusEnum.approved.value
+    user.requested_role = AccountTypesEnum.admin.value
+    user.save()
+
+
+def assign_role_based_groups(user):
+    already_assigned_to_group = user.groups.filter(name=user.requested_role).exists()
+    if user.approval_status == ProfileApprovalStatusEnum.approved.value and not already_assigned_to_group:
+        group_creator = RequestUserContext.get_current_user()
+        group, created = CustomGroup.objects.get_or_create(name=user.requested_role, group_creator=group_creator)
+        user.groups.add(group)
--- django_school_management/accounts/signals.py
+++ django_school_management/accounts/signals.py
@@ -1,19 +1,22 @@
-from Tools.demo.mcast import sender
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
+from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 
-from .constants import ProfileApprovalStatusEnum, AccountTypesEnum
-from .models import CommonUserProfile
+from django_school_management.accounts.request_context import RequestUserContext
+from django_school_management.accounts.services.auth import handle_superuser_creation, \
+    create_profile_for_approved_account, assign_role_based_groups
 
 
 User = get_user_model()
 
-def create_profile_for_approved_account(user):
-    if user.approval_status == ProfileApprovalStatusEnum.approved.value:
-        profile, created = CommonUserProfile.objects.get_or_create(user=user)
-        return profile, created
-    return None, None
+@receiver(user_logged_in)
+def set_user_on_login(sender, request, user, **kwargs):
+    RequestUserContext.set_current_user(user)
+
+@receiver(user_logged_out)
+def clear_user_on_logout(sender, request, user, **kwargs):
+    RequestUserContext.clear_current_user()
 
 
 @receiver(post_save, sender=get_user_model())
@@ -22,9 +25,8 @@ def assign_all_permissions_to_superuser(sender, instance, created, **kwargs):
     user_profile = getattr(user, 'profile', None)
 
     if instance.is_superuser and not user_profile:
-        create_profile_for_approved_account(user)
-        user.approval_status = ProfileApprovalStatusEnum.approved.value
-        user.requested_role = AccountTypesEnum.admin.value
-        user.save()
+        handle_superuser_creation(user)
     elif created and not user_profile:
         create_profile_for_approved_account(user)
+
+    assign_role_based_groups(user)
