aab44086e9e8f0a3cb35e75dee0686723ee16d69
Tarek Monwer <tarek.monwer@niftycoders.com>
2026-02-25 17:44:46 +0600

    monitoring setup [wip]

--- django_school_management/students/api/urls.py
+++ django_school_management/students/api/urls.py
@@ -3,9 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import StudentViewSet, AdmissionStudentViewSet
 
 router = DefaultRouter()
-# Main resource at '' so URL is /api/v1/students/ not /api/v1/students/students/
-router.register(r'', StudentViewSet, basename='student')
 router.register(r'applications', AdmissionStudentViewSet, basename='admission')
+router.register(r'', StudentViewSet, basename='student')
 
 app_name = 'students_api'
 
